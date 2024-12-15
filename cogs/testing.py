097313f1a358ec0aba760f0afd9197f158dd3fd0
AsherSSB <gromanenghi04@gmail.com>
2024-12-15 01:40:48 -0700

    added barb and nomad lore

--- cogs/testing.py
+++ cogs/testing.py
@@ -1,17 +1,22 @@
 import discord
 from discord.ext import commands
 from custom.weapons import Sword, Shield
+from custom.stattable import Nomad, Barbarian
 
 class Testing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.app_commands.command(name="statprint")
-    async def stattest(self, interaction:discord.Interaction):
-        sword = Sword()
-        shield = Shield(10)
-        await interaction.response.send_message(f"{sword}\n{shield}")
+    @discord.app_commands.command(name="nomadtest")
+    async def nomadtest(self, interaction:discord.Interaction):
+        nomad = Nomad()
+        await interaction.response.send_message(f"{nomad}")
 
+    @discord.app_commands.command(name="barbtest")
+    async def nomadtest(self, interaction:discord.Interaction):
+        nomad = Barbarian()
+        await interaction.response.send_message(f"{nomad}")
+    
 
 async def setup(bot):
     await bot.add_cog(Testing(bot))
--- custom/stattable.py
+++ custom/stattable.py
@@ -5,16 +5,51 @@ class StatTable():
         self.dex = dex
         self.int = int
         self.att = att
+
+    def __str__(self):
+        return f"""Stats:
+Strength: {self.str}
+Will: {self.wil}
+Dexterity: {self.dex}
+Intelligence: {self.int}
+Attunement: {self.att}"""
     
 class Nomad():
     def __init__(self):
-        self. stats = StatTable(12, 14, 10, 8, 8)
+        self.stats = StatTable(11, 14, 11, 8, 8)
 
+    def __str__(self):
+        return f"""
+# Nomad
+
+Nomads are lone wanderers, forged by their hardships. These eduring \
+and well-rounded survivalists seek adventure to test their strength and \
+resolve. Whether by the bite of a heavy sword or the thrill of navigating \
+treacherous lands, they are driven by the desire to prove themselves against \
+the challenges of the world. With few ties to any one place, they embrace a \
+life of freedom, their resourcefulness and resilience their greatest assets. \
+Adaptable and tough, the Nomad is a true testament to endurance, ever seeking \
+the next trial that will push them to their limits.
+
+Base {self.stats}"""
 
 class Barbarian():
     def __init__(self):
         self.stats = StatTable(16, 13, 7, 6, 10)
 
+    def __str__(self):
+        return f"""
+# Barbarian
+
+Barbarians are the embodiment of unrelenting rage and raw power, their very \
+existence defined by the relentless pursuit of dominance. Driven by a primal \
+hunger for conquest, they are fueled by an insatiable thirst for violence and \
+destruction, their battle cries echoing across the lands. With every swing of \
+their weapons, they carve through their enemies, \
+their fury unstoppable. These fearless warriors know no mercy, maiming and \
+slaughtering all who dare to stand in their path.
+
+Base {self.stats}"""
 
 class Bard():
     def __init__(self):
