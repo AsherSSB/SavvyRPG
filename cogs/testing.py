8c3a3c8597ee2c45017e890b9df5ec8cc2c9c61c
AsherSSB <gromanenghi04@gmail.com>
2024-12-25 15:09:26 -0700

    added style selection button to gear test

--- cogs/testing.py
+++ cogs/testing.py
@@ -74,7 +74,7 @@ class Testing(commands.Cog):
         if view.choice == -1:
             await interaction.delete_original_response()
         else:
-            await interaction.edit_original_response(content=f"You selected: {view.choice}")
+            await interaction.edit_original_response(content=f"You selected: {view.labels[view.choice]}")
             await asyncio.sleep(6.0)
             await interaction.delete_original_response()
 
@@ -83,11 +83,11 @@ class Testing(commands.Cog):
 class GearSelect(discord.ui.Select):
     def __init__(self):
         super().__init__(max_values=1)
-        self.add_option(label="Head", value="0")
-        self.add_option(label="Chest", value="1")
-        self.add_option(label="Hands", value="2")
-        self.add_option(label="Legs", value="3")
-        self.add_option(label="Feet", value="4")
+        self.add_option(label="Head", value="0", emoji="🧢")
+        self.add_option(label="Chest", value="1", emoji="👕")
+        self.add_option(label="Hands", value="2", emoji="🧤")
+        self.add_option(label="Legs", value="3", emoji="👖")
+        self.add_option(label="Feet", value="4", emoji="👢")
 
     async def callback(self, interaction):
         self.view.choice = int(self.values[0])
@@ -103,16 +103,16 @@ class ButtonGearView(discord.ui.View):
         self.event = asyncio.Event()
         self.method = 0
         self.style = 0
-        self.gear_emojis = ["🧢","👕","🧤","👖","👢"]
+        self.labels = ["Head","Chest","Hands","Legs","Feet"]
         self.buttons = []
-        self.select = GearSelect()
+        self.select: discord.SelectMenu = GearSelect()
 
         button_dict = [
-            {"label": "Head", "style": discord.ButtonStyle.blurple, "emoji": "🧢"}, 
-            {"label": "Chest", "style": discord.ButtonStyle.blurple, "emoji": "👕"},
-            {"label": "Hands", "style": discord.ButtonStyle.blurple, "emoji": "🧤"},
-            {"label": "Legs", "style": discord.ButtonStyle.blurple, "emoji": "👖"}, 
-            {"label": None, "style": discord.ButtonStyle.blurple, "emoji": "👢"}
+            {"label": "Head", "style": discord.ButtonStyle.gray, "emoji": "🧢"}, 
+            {"label": "Chest", "style": discord.ButtonStyle.gray, "emoji": "👕"},
+            {"label": "Hands", "style": discord.ButtonStyle.gray, "emoji": "🧤"},
+            {"label": "Legs", "style": discord.ButtonStyle.gray, "emoji": "👖"}, 
+            {"label": "Feet", "style": discord.ButtonStyle.gray, "emoji": "👢"}
         ]
         
         # Create and add buttons
@@ -152,8 +152,35 @@ class ButtonGearView(discord.ui.View):
             await self.swap_to_buttons()
         await interaction.response.defer()
 
+    @discord.ui.button(label="Swap Styles", style=discord.ButtonStyle.green, row=2)
+    async def swap_style_button(self, interaction: discord.Interaction, button):
+        if self.style == 0:
+            self.style = 1
+            await self.remove_button_text()
+            await self.remove_select_text()
+        else:
+            self.style = 0
+            await self.add_button_text()
+            await self.add_select_text()
+        await interaction.response.defer()
+        await self.interaction.edit_original_response(view=self)
+
+    async def add_select_text(self):
+        for i, option in enumerate(self.select.options):
+            option.label = self.labels[i]
+
+    async def add_button_text(self):
+        for i, button in enumerate(self.buttons):
+            button.label = self.labels[i]
     
-    
+    async def remove_button_text(self):
+        for button in self.buttons:
+            button.label = ""
+
+    async def remove_select_text(self):
+        for option in self.select.options:
+            option.label = "⠀"
+
     async def swap_to_buttons(self):
         self.remove_item(self.select)
         for button in self.buttons:
