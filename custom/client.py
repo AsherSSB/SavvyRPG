e5f2d75c241552c5cbfed3735ea36dbb4d5d9c26
AsherSSB <gromanenghi04@gmail.com>
2025-01-02 19:27:27 -0700

    inventory is setting and retrieving as expected

--- custom/client.py
+++ custom/client.py
@@ -2,30 +2,30 @@ import os
 import discord
 from discord.ext import commands
 
+
 class Client(commands.Bot):
-    
+
     def __init__(self):
         super().__init__(command_prefix="!", intents=discord.Intents.all())
 
     async def setup_hook(self) -> None:
         cogsdir:str = "./cogs"
-        
+
         for filename in os.listdir(cogsdir):
             if filename.endswith('.py'):
                 await self.load_extension(f"cogs.{filename[:-3]}")
 
-        await self.tree.sync() # This is global
-
+        await self.tree.sync()  # This is global
 
     async def close(self):
         # do your cleanup here
         for extension in list(self.extensions.keys()):
-            self.unload_extension(extension)
+            await self.unload_extension(extension)
 
         for cog in self.cogs.values():
             if hasattr(cog, 'cog_unload'):
                 await cog.cog_unload()
-        
+
         await super().close()
 
-        
+
