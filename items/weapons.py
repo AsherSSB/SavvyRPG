0b52eaecbe37711d0633cba8dc2d5b1b799e5ef4
AsherSSB <gromanenghi04@gmail.com>
2025-01-03 19:09:23 -0700

    added fists class

--- items/weapons.py
+++ items/weapons.py
@@ -2,9 +2,15 @@ from custom.gear import Weapon
 from custom.combat.cooldown_base_classes import SingleTargetAttack, WeaponStatTable
 
 
+class Fists(Weapon):
+    def __init__(self):
+        stats = WeaponStatTable(dmg=8, spd=1, rng=1, cc=0.1, cm=1.5, acc=0.9, scalar=0.3, stat="str")
+        self.cooldown = SingleTargetAttack(name="Punch", emoji="�", stats=stats, acted="punched", entities=None)
+
+
 class Greatsword(Weapon):
     def __init__(self):
-        stats = WeaponStatTable(dmg=10, spd=1, rng=2, cc=0.2, cm=2.0, acc=0.8, scalar=0.2, stat="str")
+        stats = WeaponStatTable(dmg=13, spd=1, rng=2, cc=0.2, cm=2.0, acc=0.8, scalar=0.2, stat="str")
         self.cooldown = SingleTargetAttack(name="Slash", emoji="⚔", stats=stats, acted="slashed", entities=None)
 
 # • Sword + Shield
