db38f7d2a2638fb3c0babb38a95cd21bea0b5be9
AsherSSB <gromanenghi04@gmail.com>
2025-01-09 11:37:34 -0700

    working on rogue cooldowns

---
+++ custom/combat/rogue/cooldowns.py
@@ -0,0 +1,4 @@
+from custom.combat.cooldown_base_classes import SingleTargetAttack, AOEAttack, Cooldown, WeaponStatTable, MovingSingleTargetAttack, SingleTargetStatus
+from custom.combat.entities import Entity, EntitiesInfo
+
+
