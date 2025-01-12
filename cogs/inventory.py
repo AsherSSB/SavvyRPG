a3534fe328cac20212ba531ecde178773d02524a
AsherSSB <gromanenghi04@gmail.com>
2025-01-11 18:15:47 -0700

    sendinv command

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -2,3 +2,17 @@ import discord
 from discord.ext import commands
 
 
+class Inventory(commands.Cog):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @discord.app_commands.command(name="sendinv")
+    async def send_inventory_menu(self, interaction: discord.Interaction):
+        pass
+
+
+async def setup(bot):
+    await bot.add_cog(Inventory(bot))
+
+
