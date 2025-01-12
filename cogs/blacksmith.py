97340bc9e105d01ab4ea21555688791bc446e990
AsherSSB <gromanenghi04@gmail.com>
2025-01-11 18:26:55 -0700

    created blacksmith view

--- cogs/blacksmith.py
+++ cogs/blacksmith.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 class Blacksmith(commands.Cog):
     def __init__(self, bot):
@@ -9,6 +10,35 @@ class Blacksmith(commands.Cog):
     @discord.app_commands.command(name="sendblacksmith")
     async def send_blacksmith_menu(self, interaction: discord.Interaction):
         pass
+
+
+class BlacksmithView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.choice: int
+        self.interaction: discord.Interaction
+        self.event = asyncio.Event()
+
+    @discord.ui.button(label="Back", style=discord.ButtonStyle.red)
+    async def back_button(self, interaction: discord.Interaction, button):
+        self.interaction = interaction
+        self.choice = -1
+        self.event.set()
+
+    @discord.ui.button(label="Buy", style=discord.ButtonStyle.green)
+    async def buy_button(self, interaction: discord.Interaction, button):
+        self.interaction = interaction
+        self.choice = 0
+        self.event.set()
+
+    @discord.ui.button(lable="Sell", style=discord.ButtonStyle.green)
+    async def sell_button(self, interaction: discord.Interaction, button):
+        self.interaction = interaction
+        self.choice = 1
+        self.event.set()
+
+    async def wait(self):
+        await self.event.wait()
 
 
 async def setup(bot):
