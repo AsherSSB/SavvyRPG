202fede4f2fe0f908b7f4997ec64287cdd3e7a14
AsherSSB <gromanenghi04@gmail.com>
2025-01-14 17:11:42 -0700

    seperated test command and added continue view

--- cogs/gear.py
+++ cogs/gear.py
@@ -15,6 +15,9 @@ class GearMenu(commands.Cog):
         self.db = Database(self.bot)
 
     @discord.app_commands.command(name="gearmenu")
+    async def test_send_equip_slots_menu(self, interaction: discord.Interaction):
+        await self.send_equip_slots_menu(interaction)
+
     async def send_equip_slots_menu(self, interaction: discord.Interaction):
         view = ButtonGearView(interaction)
         await interaction.response.send_message("Gear", view=view)
@@ -38,13 +41,17 @@ class GearMenu(commands.Cog):
         attr = slots[slot_index]
         gear = getattr(loadout, attr, None)
         equippables = [item for item in inventory if isinstance(item, type(gear))]
+        if len(equippables) < 1:
+            view = ContinueView()
+            await interaction.response.send_message("You have no items of that kind to equip!", view=view)
+            await view.wait()
+            return await self.send_equip_slots_menu(view.interaction)
         # returns interaction back to main menus
         return await self.send_equip_menu(interaction, loadout, equippables)
 
     async def send_equip_menu(self, interaction, loadout: Loadout, inventory: list[Item]):
         embed = InventoryEmbed(inventory=inventory)
         view = InventoryView(interaction=interaction, inventory=inventory, embed=embed)
-        # currently throws error for options not being populated when equippables is len 0
         await interaction.response.send_message(content="Equippables", view=view, embed=embed)
         await view.wait()
         if view.choice == -1:
@@ -58,6 +65,21 @@ class GearMenu(commands.Cog):
 
     async def cog_unload(self):
         await self.cleanup()
+
+
+class ContinueView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.event = asyncio.Event()
+        self.interaction: discord.Interaction
+
+    @discord.ui.button(label="Continue", style=discord.ButtonStyle.green)
+    async def continue_button(self, interaction: discord.Interaction, button):
+        self.interaction = interaction
+        self.event.set()
+
+    async def wait(self):
+        await self.event.wait()
 
 
 async def setup(bot):
